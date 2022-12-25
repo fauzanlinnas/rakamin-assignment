d35006e3e00af7c83933746e9b107069e20fc78e
fauzan <fauzanlinnas@gmail.com>
2022-12-25 19:13:52 +0700

    feat: redirect to v1

--- src/index.js
+++ src/index.js
@@ -5,13 +5,19 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { AuthProvider } from "./context/AuthProvider";
 import { BoardsProvider } from "./context/BoardsProvider";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <AuthProvider>
       <BoardsProvider>
-        <App />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/v1" element={<App />} />
+            <Route path="/" element={<Navigate to="/v1" />} />
+          </Routes>
+        </BrowserRouter>
       </BoardsProvider>
     </AuthProvider>
   </React.StrictMode>
